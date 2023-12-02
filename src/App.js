554bf44a2e0fcf68fe3972118fb950d766b4133d
Nikita kadiwike <ninavenikita492@gmail.com>
2023-12-02 20:56:08 +0530

    play box ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='tic-tac-toe-title'>Tic Tac Toe</h1>
+<div className='container'>
+     <div className='row'>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+     </div>
+
+     <div className='row'>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+     </div>
+     
+     <div className='row'>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+     </div> 
+
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
